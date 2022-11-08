141dda6ac50049b96d9cb470883207ea26936c30
Arthur Mochiuti Braghetto <arthurmb@gmail.com>
2022-11-08 15:13:41 -0300

    ripe_atlas_probe removed from repo

--- ripe_atlas_probe/test-final-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm --privileged -v ~/.docker:/root/.docker -v ~/Code/ha-addons/ripe_atlas_probe:/data homeassistant/amd64-builder --test --target /data --all
--- ripe_atlas_probe/test-local-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build --build-arg BUILD_FROM="homeassistant/amd64-base-debian:latest" -t ripe_probe .
